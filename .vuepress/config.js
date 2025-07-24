80ba1a17403ade02de095e96b04a22478c638b07
Janik von Rotz <login@janikvonrotz.ch>
2025-07-24 16:29:30 +0200

    fix(docs): add support for VuePress documentation

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,29 +1,54 @@
-import { defaultTheme } from 'vuepress'
-import { searchPlugin } from '@vuepress/plugin-search'
-import { defineUserConfig } from 'vuepress'
-
+import { viteBundler } from "@vuepress/bundler-vite";
+import { defaultTheme } from "@vuepress/theme-default";
+import { searchPlugin } from "@vuepress/plugin-search";
+import { shikiPlugin } from "@vuepress/plugin-shiki";
+import { defineUserConfig } from "vuepress";
 
 export default defineUserConfig({
-    lang: 'en-US',
-    title: 'Nextcloud Build',
-    description: 'The Mint System Nextcloud development environment.',
-    head: [
-        ['link', { rel: 'icon', href: '/icon.png' }]
+  bundler: viteBundler(),
+  lang: "en-US",
+  title: "Nextcloud Build",
+  description: "The Mint System Nextcloud development environment.",
+  head: [["link", { rel: "icon", href: "/icon.png" }]],
+  pagePatterns: ["**/*.md", "!.vuepress", "!node_modules", "!tmp", "!venvmain"],
+  theme: defaultTheme({
+    logo: "/icon.png",
+    repo: "mint-system/nextcloud-build",
+    docsBranch: "main",
+    editLink: true,
+    navbar: [
+      { text: "Home", link: "/" },
+      { text: "Help", link: "/task" },
+      { text: "Mint System", link: "https://www.mint-system.ch" },
     ],
-    theme: defaultTheme({
-        logo: '/icon.png',
-        repo: 'mint-system/nextcloud-build',
-        docsBranch: 'main',
-        editLink: true,
-        navbar: [
-            { text: 'Home', link: '/' },
-            { text: 'Help', link: '/task' },
-            { text: 'Mint System', link: 'https://www.mint-system.ch' }
-        ],
+  }),
+  plugins: [
+    searchPlugin({
+      maxSuggestions: 10,
     }),
-    plugins: [
-        searchPlugin({
-            maxSuggestions: 10
-        }),
-    ],
-})
+    shikiPlugin({
+      theme: "catppuccin-latte",
+      langs: [
+        "bash",
+        "yml",
+        "yaml",
+        "json",
+        "css",
+        "html",
+        "xml",
+        "groovy",
+        "py",
+        "python",
+        "sql",
+        "powershell",
+        "txt",
+        "csv",
+        "mermaid",
+        "md",
+        "markdown",
+        "toml",
+        "php",
+      ],
+    }),
+  ],
+});
